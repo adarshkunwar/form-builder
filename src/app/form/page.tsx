c1624c8f748369ebc1e21f6592953f124df9eba9
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-16 00:59:16 +0545

    refactor: update component name

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,20 +1,25 @@
+"use client";
 import FieldOptionCollection from "@/components/HOC/fieldOptionCollection";
-import FieldOptionArranger from "@/components/pageComponents/form/fieldList";
+import FieldRowArranger from "@/components/pageComponents/form/fieldRowArranger";
 import FieldOptionRenderer from "@/components/pageComponents/form/fieldSelector";
+import { closestCorners, DndContext } from "@dnd-kit/core";
 
 const FormPage = () => {
   return (
-    <div className="gap-4 px-20 py-2 md:grid md:grid-cols-12">
-      <div className="col-span-2">
-        <FieldOptionCollection />
+    <DndContext collisionDetection={closestCorners}>
+      <div className="gap-4 px-20 py-2 md:grid md:grid-cols-12">
+        <div className="col-span-2">
+          <FieldOptionCollection />
+        </div>
+        <div className="col-span-4">
+          <FieldRowArranger />
+          {/* <FieldOptionArranger /> */}
+        </div>
+        <div className="col-span-6">
+          <FieldOptionRenderer />
+        </div>
       </div>
-      <div className="col-span-4">
-        <FieldOptionArranger />
-      </div>
-      <div className="col-span-6">
-        <FieldOptionRenderer />
-      </div>
-    </div>
+    </DndContext>
   );
 };
 
