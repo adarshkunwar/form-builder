a2309ba04baec612481c8bda35565d7db6a4f5d2
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-16 13:04:55 +0545

    feat: Let user chose between the type of output

---
+++ src/components/pageComponents/form/FormOutputSelector.tsx
@@ -0,0 +1,45 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FieldOptionRenderer from "./fieldSelector";
+import FormJson from "./formJson";
+
+const FormOutputSelector = () => {
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger value="output">OUTPUT</TabsTrigger>
+        <TabsTrigger value="json">JSON</TabsTrigger>
+        <TabsTrigger value="code">CODE</TabsTrigger>
+      </TabsList>
+      <TabsContent value="output">
+        <Card>
+          <CardHeader>
+            <CardTitle>output</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <FieldOptionRenderer />
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="json">
+        <Card>
+          <CardHeader>
+            <CardTitle>JSON</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <FormJson />
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="code">
+        <Card>
+          <CardContent className="space-y-2">
+            <FormJson />
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default FormOutputSelector;
